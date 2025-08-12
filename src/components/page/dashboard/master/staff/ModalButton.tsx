544e20902166f08ab2d4d6eedf580923ffcf96ac
FaiqSr <faiqsr13@gmail.com>
2025-08-12 14:07:18 +0700

    change background color modal

--- src/components/page/dashboard/master/staff/ModalButton.tsx
+++ src/components/page/dashboard/master/staff/ModalButton.tsx
@@ -18,11 +18,11 @@ const ModalButton = () => {
 
       {isModalOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 flex items-center justify-center p-4"
+          className="fixed inset-0 backdrop-blur-sm bg-opacity-50 z-40 flex items-center justify-center p-4"
           onClick={closeModal}
         >
           <div
-            className="bg-white rounded-xl shadow-2xl w-full max-w-md p-6 transform transition-all duration-300 scale-95 opacity-0 animate-fade-in-scale"
+            className="bg-white rounded-sm shadow-2xl w-full max-w-md p-6 transform transition-all duration-300 scale-95 opacity-0 animate-fade-in-scale"
             onClick={(e) => e.stopPropagation()}
           >
             <section className="flex items-center justify-between pb-4 border-b border-slate-200">
